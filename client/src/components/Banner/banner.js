712389973fdb40d5dee22ce675f2488a9ac34acb
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-03-14 16:49:32 -0400

    api working

--- client/src/components/Banner/banner.js
+++ client/src/components/Banner/banner.js
@@ -1,13 +1,25 @@
   import React from "react";
 import "./style.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Banner(props) {
     return (
       <div>
         <div className="banner">
-          <div className="container">
-            <div className="row">
+          {/* <div className="container"> */}
+            <div className="row rowone">
               <h1 className="pageTitle">Search any piece of art!</h1>
+            </div>
+            <div className="container containersearch">
+            <div className="row rowtwo">
+              <div className="col-md-4 colone">
+              <input className="form-control searchform"  placeholder="Search">
+                </input>
+              </div>
+              <div className="col-md-4">
+               <button className="btn srchbtn btn-primary"><FontAwesomeIcon icon={faSearch} /></button> 
+              </div>
             </div>
           </div>
         </div>
--- client/src/pages/frontpage.js
+++ client/src/pages/frontpage.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
-import API from "../utils/API";
+import axios from "axios";
 import Banner from '../components/Banner/banner';
 
 function FrontPage() {
 
+    // function Search() {
+    //     API.getArt
+    // }
 
-
+    useEffect(() => {
+        axios
+        .get(`https://api.artic.edu/api/v1/artworks/search?q=starrynight`)
+        .then(res => console.log(res.data))
+      })
 
     return (
     <div>
+
 <Banner />
     </div>
     )
