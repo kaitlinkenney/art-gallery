fbf5f32dc0250d240c40fa60f8aff04ccaff70f4
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-03-22 12:29:47 -0400

    api changed again

--- client/src/pages/frontpage.js
+++ client/src/pages/frontpage.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Banner from '../components/Banner/banner';
 import { Link } from "react-router-dom";
 import Result from "../components/Result/result";
+import rest from "restler";
 
 
 function FrontPage() {
@@ -13,9 +14,16 @@ function FrontPage() {
     const [list, setList] = useState([]);
 
     useEffect(() => {
-        axios
-            .get(`https://openaccess-api.clevelandart.org/api/artworks?q=sunflowers`)
-            .then(res => setApi(res.data))
+        rest.get("https://api.harvardartmuseums.org/object", {
+    query: {
+        apikey: "13426f78-76b7-456b-85d9-15e3e4d1875a",
+        title: "dog",
+        // fields: "objectnumber,title,dated",
+    }
+}).on("complete", function(data, response) {
+    console.log(data);
+});
+
     }, [])
     
     console.log(api)
